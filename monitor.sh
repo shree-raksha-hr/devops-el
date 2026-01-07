c598d8600d7b12fb7797fcfb693b104de6194f33
shree-raksha-hr <shreeraksahr@gmail.com>
2026-01-07 21:04:54 +0530

    v3

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BACKEND_URL=$(minikube service devops-el-lb --url -n backend)
+error_count=0
+for i in {1..3}; do
+  response=$(curl -s -o /dev/null -w '%{http_code}' $BACKEND_URL/api/status)
+  if [ $response -ne 200 ]; then
+    ((error_count++))
+  fi
+  sleep 10
+done
+if [ $error_count -gt 1 ]; then
+  kubectl patch svc devops-el-lb -p '{"spec":{"selector":{"color":"blue"}}}' -n backend
+  kubectl scale deployment/devops-el-green --replicas=0 -n backend
+fi
